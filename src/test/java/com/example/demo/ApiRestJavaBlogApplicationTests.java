948aed324f5aae44cdd5eb41f98bdf84f115da9a
Pc <alxzwl@gmail.com>
2022-05-19 19:55:27 -0500

    :tada: Inicio de un Api Rest en Java -Blog-

---
+++ src/test/java/com/example/demo/ApiRestJavaBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestJavaBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
