2c0a4c7b294b4b22d2a1d0b1cd5e0ec923b03c95
Pc <alxzwl@gmail.com>
2022-05-19 23:44:30 -0500

    :sparkles: Creacion de la interfaz PublicacionServicio

---
+++ src/main/java/com/sistema/blog/services/PublicacionServicio.java
@@ -0,0 +1,8 @@
+package com.sistema.blog.services;
+
+import com.sistema.blog.dto.PublicacionDTO;
+
+public interface PublicacionServicio {
+
+	public PublicacionDTO crearPublicacion(PublicacionDTO publicacionDTO);
+}
